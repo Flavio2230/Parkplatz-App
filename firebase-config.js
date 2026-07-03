93ea75198a86b02583cd6f2f65764d7f164611a8
Flavio2230 <flavio.wuethrich@sunrise.ch>
2026-07-03 10:12:11 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.firebaseConfig = {
+  apiKey: "AIzaSyAUV02bxhvHRQwnd8gCi-avdxnucu2hQco",
+  authDomain: "parkplatzreservation.firebaseapp.com",
+  projectId: "parkplatzreservation",
+  storageBucket: "parkplatzreservation.firebasestorage.app",
+  messagingSenderId: "501800007237",
+  appId: "1:501800007237:web:6510bcd7883dedf296d52c"
+};
